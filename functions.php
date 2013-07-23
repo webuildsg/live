e09061a039cfa0fbb0fa4247dd859c210b0ed9e3
Sayanee <sayanee@gmail.com>
2013-07-23 16:10:00 +0800

    commented out custom scripts

--- functions.php
+++ functions.php
@@ -136,19 +136,19 @@ add_action( 'wp_enqueue_scripts', 'webuild_scripts' );
 /**
  * Custom template tags for this theme.
  */
-require get_template_directory() . '/inc/template-tags.php';
+// require get_template_directory() . '/inc/template-tags.php';
 
 /**
  * Custom functions that act independently of the theme templates.
  */
-require get_template_directory() . '/inc/extras.php';
+// require get_template_directory() . '/inc/extras.php';
 
 /**
  * Customizer additions.
  */
-require get_template_directory() . '/inc/customizer.php';
+// require get_template_directory() . '/inc/customizer.php';
 
 /**
  * Load Jetpack compatibility file.
  */
-require get_template_directory() . '/inc/jetpack.php';
+// require get_template_directory() . '/inc/jetpack.php';
