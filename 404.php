05ea295d5d45c30f0b516a895bb50e442bbdf35a
Sayanee <sayanee@gmail.com>
2013-09-22 07:38:36 +0800

    deleted master branch contents form previous wordpress theme

--- 404.php
+++
@@ -1,8 +0,0 @@
-<?php get_header(); ?>
-<?php get_sidebar(); ?>
-
-<section>
-	<?php get_template_part( 'no-results', 'search' ); ?>
-</section>
-
-<?php get_footer(); ?>
--- searchform.php
+++
@@ -1,4 +0,0 @@
-<form method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
-	<input type="search" class="field" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" id="s" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'webuild' ); ?>" />
-
-</form>
